27b18a82dfd4b6c90f3c57f9b7bd039439b94f08
AGRIDJAR <29222621+AGRIDJAR@users.noreply.github.com>
2024-07-05 10:54:28 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React JS CRUD Opertations</h1>
+      <BrowserRouter>
+        <Routes>// navigate to employee home page(/)
+          <Route path='/' element={<EmpListing />}></Route>//routing to home page after every exeucation
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
+
+          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+
+}
+
+export default App;
